566638e2f28559d1fabffd7a13a10ed5254721ca
Wilhelmina <willemijnsprangers@gmail.com>
2021-12-09 14:42:26 +0100

    2021 day1

---
+++ 2021/Day1/Part1.hs
@@ -0,0 +1,11 @@
+module Day1.Part1 where
+
+main :: IO ()
+main = do
+    input <- readFile "input.txt"
+    print . calculate 0 . map read . lines $ input
+
+calculate :: Int -> [Int] -> Int
+calculate _ [] = -1
+calculate last (x:xs) | x > last = 1 + calculate x xs
+                      | otherwise = calculate x xs
---
+++ 2021/Day1/Part2.hs
@@ -0,0 +1,12 @@
+module Day1.Part2 where
+
+main :: IO ()
+main = do
+    input <- readFile "input.txt"
+    print . calculate 0 . map read . lines $ input
+
+calculate :: Int -> [Int] -> Int
+calculate _ [] = -1
+calculate last xs | a > last = 1 + calculate a (tail xs)
+                  | otherwise = calculate a (tail xs)
+    where a = sum (take 3 xs)
